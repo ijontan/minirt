67499d39849e9bbeeb5cbd55b1791a3a8e6b64ed
ijon <itan@student.42kl.edu.my>
2023-09-06 15:19:42 +0800

    update some octree stuff

--- includes/share.h
+++ includes/share.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   share.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsoo <rsoo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/29 13:22:20 by itan              #+#    #+#             */
-/*   Updated: 2023/09/04 16:59:32 by rsoo             ###   ########.fr       */
+/*   Updated: 2023/09/06 15:18:51 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHARE_H
 # define SHARE_H
 
-#include <fcntl.h>
+# include <fcntl.h>
 
 typedef struct s_vec3
 {
@@ -82,7 +82,7 @@ typedef struct s_light_src
 {
 	t_vec3			position;
 	float			ratio;
-	t_material		material; // bonus
+	t_material material; // bonus
 }					t_light_src;
 
 typedef struct s_sphere
@@ -106,8 +106,8 @@ typedef struct s_plane
 	float			t;
 }					t_plane;
 
-t_plane	plane_new(t_vec3 point, t_vec3 dir, t_material material);
-t_vec3	plane_intersect(t_plane *plane, t_ray *ray);
+t_plane				plane_new(t_vec3 point, t_vec3 dir, t_material material);
+t_vec3				plane_intersect(t_plane *plane, t_ray *ray);
 
 typedef struct s_cylinder
 {
@@ -121,62 +121,91 @@ typedef struct s_cylinder
 t_vec3				cylinder_intersect(t_cylinder *cylinder, t_ray *ray);
 t_vec3				cylinder_normal(t_cylinder *cylinder, t_vec3 point);
 
-typedef struct	s_atof
+typedef struct s_atof
 {
-	float	result;
-	float 	power;
-	int		sign;
-	int		i;
-}				t_atof;
+	float			result;
+	float			power;
+	int				sign;
+	int				i;
+}					t_atof;
 
 // Parsing
 typedef struct s_parse
 {
-	int			infile_fd;
-	int 		mand_flag[6];
-	char		*obj_type[6];
-	void		(*func_ptr[6])(struct s_parse*);
-	char		**info;
-	int			rgb[3];
-	float		coords[3];
-	t_atof		atof;
-	t_amb_light amb_light;
-	t_cam		camera;
-	t_light_src	light_source;
-	t_sphere	sphere;
-	t_plane		plane;
-	t_cylinder	cylinder;
-}				t_parse;
+	int				infile_fd;
+	int				mand_flag[6];
+	char			*obj_type[6];
+	void			(*func_ptr[6])(struct s_parse *);
+	char			**info;
+	int				rgb[3];
+	float			coords[3];
+	t_atof			atof;
+	t_amb_light		amb_light;
+	t_cam			camera;
+	t_light_src		light_source;
+	t_sphere		sphere;
+	t_plane			plane;
+	t_cylinder		cylinder;
+}					t_parse;
 
 // parsing.c
-bool parse_rt_file(char *infile, t_parse *parse_info);
+bool				parse_rt_file(char *infile, t_parse *parse_info);
 
 // parsing_utils.c
-void	free_2darray(char **s);
-float	ft_atof(char *s, t_parse *p);
+void				free_2darray(char **s);
+float				ft_atof(char *s, t_parse *p);
 
 // parse_objects.c
-void 	parse_sphere(t_parse *p);
-void 	parse_plane(t_parse *p);
-void 	parse_cylinder(t_parse *p);
+void				parse_sphere(t_parse *p);
+void				parse_plane(t_parse *p);
+void				parse_cylinder(t_parse *p);
 
 // parse_environment.c
-void 	parse_ambient_lighting(t_parse *p);
-void 	parse_camera(t_parse *p);
-void 	parse_lighting(t_parse *p);
+void				parse_ambient_lighting(t_parse *p);
+void				parse_camera(t_parse *p);
+void				parse_lighting(t_parse *p);
 
 // parse_attr.c
-bool	check_rgb(char *s, t_parse *p);
-bool	check_normalized(char *s, t_parse *p);
-t_vec3	assign_norm_vec(t_parse *p);
-t_vec3	parse_coordinates(char *s, t_parse *p);
+bool				check_rgb(char *s, t_parse *p);
+bool				check_normalized(char *s, t_parse *p);
+t_vec3				assign_norm_vec(t_parse *p);
+t_vec3				parse_coordinates(char *s, t_parse *p);
 
 // error_handling.c
-void	exit_parse(char **info, char *s, char c);
-void	check_line_format(char *s, t_parse *p);
+void				exit_parse(char **info, char *s, char c);
+void				check_line_format(char *s, t_parse *p);
 
 // error_handling2.c
-bool	valid_float(char *s);
-bool	valid_triplet(char *s);
+bool				valid_float(char *s);
+bool				valid_triplet(char *s);
+
+/* --------------------------------- octree --------------------------------- */
+
+typedef struct s_bounding_box
+{
+	t_vec3			min;
+	t_vec3			max;
+}					t_bounding_box;
+
+# define OCTREE_MAX_DEPTH 10
+# define OCTREE_MAX_OBJECTS 10
+# define OCTREE_MAX_NODE 8
+
+# define SPHERE 0
+# define PLANE 1
+# define CYLINDER 2
+
+typedef struct s_object
+{
+	void			*object;
+	unsigned char	type;
+}					t_object;
+
+typedef struct s_octree
+{
+	t_bounding_box	bounding_box;
+	t_list			*objects;
+	struct s_octree	**children;
+}					t_octree;
 
 #endif
---
+++ src_shared/octree/box_intersect.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   box_intersect.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 11:25:10 by itan              #+#    #+#             */
+/*   Updated: 2023/09/06 15:17:02 by itan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minirt.h"
+
+static void	swap_float(float *a, float *b)
+{
+	float	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+bool	box_intersect(t_bounding_box *box, t_ray *ray)
+{
+	float	tmin;
+	float	tmax;
+	float	tmin_tmp;
+	float	tmax_tmp;
+	int		i;
+
+	tmin = -INFINITY;
+	tmax = INFINITY;
+	i = -1;
+	while (++i < 3)
+	{
+		tmin_tmp = (box->min[i] - ray->origin[i]) / ray->dir[i];
+		tmax_tmp = (box->max[i] - ray->origin[i]) / ray->dir[i];
+		if (tmin_tmp > tmax_tmp)
+			swap_float(&tmin_tmp, &tmax_tmp);
+		if (tmin_tmp > tmin)
+			tmin = tmin_tmp;
+		if (tmax_tmp < tmax)
+			tmax = tmax_tmp;
+		if (tmin > tmax)
+			return (false);
+	}
+	return (true);
+}
