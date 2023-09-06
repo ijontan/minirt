b9fa622d98b49cc61b89674574c3f60740d4c238
ijon <itan@student.42kl.edu.my>
2023-09-06 15:29:56 +0800

    fixed now can compile

--- src_shared/octree/octree.c
+++ src_shared/octree/octree.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/06 11:31:51 by itan              #+#    #+#             */
-/*   Updated: 2023/09/06 15:19:10 by itan             ###   ########.fr       */
+/*   Updated: 2023/09/06 15:27:16 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ t_octree	*octree_new_node(t_bounding_box *box, t_list *objects)
 	node = malloc(sizeof(t_octree));
 	if (!node)
 		return (NULL);
-	node->box = box;
+	node->bounding_box = *box;
 	node->objects = objects;
 	node->children = NULL;
 	return (node);
@@ -27,6 +27,9 @@ t_octree	*octree_new_node(t_bounding_box *box, t_list *objects)
 
 t_octree	*octree_new(t_bounding_box *box, t_list *objects)
 {
+	(void)box;
+	(void)objects;
+	return (NULL);
 }
 
 void	octree_free(t_octree *node)
