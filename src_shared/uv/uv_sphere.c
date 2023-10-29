646780433289474cfff129218e246f4ebe548e99
ijontan <85871000+ijontan@users.noreply.github.com>
2023-10-29 18:07:26 +0800

    fixed flipped map

--- src_shared/uv/uv_sphere.c
+++ src_shared/uv/uv_sphere.c
@@ -6,7 +6,7 @@
 /*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 01:52:10 by itan              #+#    #+#             */
-/*   Updated: 2023/10/27 18:03:53 by itan             ###   ########.fr       */
+/*   Updated: 2023/10/29 18:05:53 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,6 @@ t_offset	uv_sphere(t_vec3 point, t_offset size)
 	(void)size;
 	theta = acos(-point.y);
 	phi = atan2(-point.z, point.x) + M_PI;
-	return ((t_offset){.x = (int)(size.x * phi * PI_DIV2), .y = (int)(size.y
-			* (1 - theta * PI_DIV))});
+	return ((t_offset){.x = (int)(size.x * (1 - phi * PI_DIV2)),
+		.y = (int)(size.y * (1 - theta * PI_DIV))});
 }
