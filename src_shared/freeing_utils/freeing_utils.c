56f188280e83c61ab5265d78aae8f030f81c364a
ijontan <85871000+ijontan@users.noreply.github.com>
2023-11-06 14:25:48 +0800

    Refactor freeing_utils.c to properly free objects and materials

--- src_shared/freeing_utils/freeing_utils.c
+++ src_shared/freeing_utils/freeing_utils.c
@@ -3,25 +3,51 @@
 /*                                                        :::      ::::::::   */
 /*   freeing_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rsoo <rsoo@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: itan <itan@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 10:39:51 by rsoo              #+#    #+#             */
-/*   Updated: 2023/11/03 10:58:28 by rsoo             ###   ########.fr       */
+/*   Updated: 2023/11/06 14:17:29 by itan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
-void del(void *content)
+void	del_light(void *content)
 {
 	if (content)
 		free(content);
 }
 
-void free_minirt(t_minirt *minirt)
+void	del_obj(void *content)
 {
-	ft_lstclear(&minirt->pt_lights, del);
-	ft_lstclear(&minirt->objects, del);
+	t_object	*obj;
+	t_material	mt;
+
+	obj = (t_object *)content;
+	if (!obj)
+		return ;
+	if (obj->type == SPHERE)
+		mt = ((t_sphere *)obj->object)->material;
+	else if (obj->type == PLANE)
+		mt = ((t_plane *)obj->object)->material;
+	else if (obj->type == CYLINDER)
+		mt = ((t_cylinder *)obj->object)->material;
+	else if (obj->type == CONE)
+		mt = ((t_cone *)obj->object)->material;
+	if (mt.texture_path)
+		free(mt.texture_path);
+	if (mt.norm_map_path)
+		free(mt.norm_map_path);
+	if (mt.spec_map_path)
+		free(mt.spec_map_path);
+	free(obj->object);
+	free(obj);
+}
+
+void	free_minirt(t_minirt *minirt)
+{
+	ft_lstclear(&minirt->pt_lights, del_light);
+	ft_lstclear(&minirt->objects, del_obj);
 	if (minirt->render_status == RENDER_DONE)
 	{
 		mlx_destroy_window(minirt->mlx, minirt->win);
